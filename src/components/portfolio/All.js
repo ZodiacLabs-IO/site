13f9121c9ff72dc2e17239788cdf3caec7fec701
Tim Paine <t.paine154@gmail.com>
2020-01-03 18:48:57 -0500

    ingest images

--- src/components/portfolio/All.js
+++ src/components/portfolio/All.js
@@ -6,14 +6,14 @@ const products = [
     description: '',
     link: '',
     longDescription: '',
-    imgPath: 'https://images.unsplash.com/photo-1520870121499-7dddb6ccbcde?auto=format&fit=crop&w=400&h=250&q=80',
+    imgPath: 'img/flyaway.jpg',
   },
   {
     label: 'FashionLab',
     description: 'FashionLab is a company that applies artificial intelligence and neural networks to help fashion brands and designers.',
     link: 'https://fashionlab.ai',
     longDescription: '',
-    imgPath: 'https://images.unsplash.com/photo-1445205170230-053b83016050?auto=format&fit=crop&w=400&h=250&q=80',
+    imgPath: 'img/fashionlab.jpg'
   },
 ];
 
