cab7f9f1767e6067f84d13d8d7d0374865b5c7bc
Tim Paine <t.paine154@gmail.com>
2020-01-04 13:20:53 -0500

    add avatar page

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -5,8 +5,10 @@ import withStyles from '@material-ui/styles/withStyles';
 import { withRouter } from 'react-router-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
+import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 
 import Topbar from './Topbar';
 import Footer from './Footer';
@@ -22,7 +24,7 @@ const styles = (theme) => ({
     overflow: 'hidden',
     backgroundSize: 'cover',
     backgroundPosition: '0 400px',
-    paddingBottom: 400,
+    paddingBottom: 100,
   },
   grid: {
     margin: `0 ${theme.spacing(2)}px`,
@@ -30,11 +32,20 @@ const styles = (theme) => ({
       width: 'calc(100% - 20px)',
     },
   },
+  gridPaddingTop: {
+    margin: `0 ${theme.spacing(2)}px`,
+    [theme.breakpoints.down('sm')]: {
+      width: 'calc(100% - 20px)',
+    },
+    marginTop: 100,
+  },
   paper: {
-    padding: theme.spacing(3),
-    margin: theme.spacing(2),
-    textAlign: 'left',
-    color: theme.palette.text.secondary,
+    display: "flex",
+    flexDirection: "column",
+    margin: "auto",
+    paddingTop: theme.spacing(3),
+    paddingBottom: theme.spacing(3),
+    alignItems: "center",
   },
   topBar: {
     display: 'flex',
@@ -47,13 +58,46 @@ const styles = (theme) => ({
   },
   outlinedButtom: {
     textTransform: 'uppercase',
-    margin: theme.spacing(1),
+    marginTop: 10,
   },
   block: {
     padding: theme.spacing(2),
     marginBottom: 20,
   },
+  avatar: {
+    width: 200,
+    height: 200,
+  }
 });
+
+
+const Team = [
+  {
+    name: "Anthony Fragiacomo",
+    title: "Title",
+    image: "test.png",
+  },
+  {
+    name: "Bryan Sudartanto",
+    title: "Title",
+    image: "test.png",
+  },
+  {
+    name: "Jeffrey Luk",
+    title: "Title",
+    image: "test.png",
+  },
+  {
+    name: "Johnny Shen",
+    title: "Title",
+    image: "test.png",
+  },  
+  {
+    name: "Tim Paine",
+    title: "Title",
+    image: "test.png",
+  },
+]
 
 
 class AboutUs extends Component {
@@ -80,25 +124,30 @@ class AboutUs extends Component {
                   </div>
                 </div>
               </Grid>
-              <Grid item xs={12}>
-                <div className={classes.topBar}>
-                  <div className={classes.block}>
-                    <Typography variant="h6" gutterBottom>Who we are</Typography>
-                    <Typography variant="body1">
-                    Some stuff about us and our roles.
-                    </Typography>
-                  </div>
-                </div>
+              <Grid container justify="center" direction="row" spacing={3}>
+              {Team.map(value => (
+              <Grid item xs={12} sm={6} md={4} key={value.name} >
+                <Paper className={classes.paper}>
+                  <Avatar alt={value.name} src={value.image} className={classes.avatar} />
+                  <Typography variant="h6">
+                    {value.name}
+                  </Typography>
+                  <Typography variant="subtitle1">
+                    {value.title}
+                  </Typography>
+                </Paper>
               </Grid>
-              <Grid alignItems="center" justify="center" container className={classes.grid}>
+              ))}
+              </Grid>
+              <Grid alignItems="center" justify="center" container className={classes.gridPaddingTop}>
                 <Grid item xs={12}>
-                  <div className={classes.topBar}>
-                    <div>
-                      <Button variant="outlined" className={classes.outlinedButtom}>
+                  <Typography variant="h6" gutterBottom>Contact Us</Typography>
+                  <Typography variant="body1" gutterBottom>
+                  Feel free to reach out to us with any questions.
+                  </Typography>
+                    <Button variant="outlined" className={classes.outlinedButtom}>
                         Email us
                       </Button>
-                    </div>
-                  </div>
                 </Grid>
               </Grid>
             </Grid>
